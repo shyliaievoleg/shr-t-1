5b188c2748226b41646267a7f8a0175a37496a8e
Oleg Shiliaev <shyliaievoleg@gmail.com>
2019-12-26 15:59:19 +0200

    git init

---
+++ app/js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+document.body.addEventListener('click', function(e) {
+
+    let accordion = e.target.closest('.e-accordion__short');
+    let onoffswitch = e.target.closest('.onoffswitch')
+
+    if (accordion) {
+        let elem = accordion.parentElement.querySelector('.e-accordion__more');
+
+        if (elem.style.display == 'flex') {
+            elem.style.display = 'none';
+        } else {
+            elem.style.display = 'flex';
+        }
+    }
+
+    if (onoffswitch) {
+        onoffswitch.classList.toggle('onoffswitch_checked');
+
+        // TODO: Switch theme
+    }
+
+});
